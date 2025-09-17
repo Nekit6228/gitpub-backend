e754292275ef47af49b67e48b342c053bca8fcae
Mykyta Petrakovskiy <nekitpetr46228@gmail.com>
2025-09-17 10:42:00 +0200

    routes login register logout

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,18 +1,51 @@
-import {  logoutUser, refreshUsersSession, requestResetToken } from "../services/auth.js";
+import {  loginUser, logoutUser, refreshUsersSession, registerUser, requestResetToken } from "../services/auth.js";
 import { resetPassword } from '../services/auth.js';
+import { ONE_DAY } from "../constants/index.js";
 
 const setupSession = (res,session) => {
-
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
 };
 
 export const registerUserController = async (req,res)=>{
+const user = await registerUser(req.body);
 
-
+res.status(201).json({
+        status: 201,
+        massage: 'Successfully registered a user!',
+        data:user,
+    });
 };
 
 
 export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
 
+  setupSession(res,session);
+
+   res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+       sessionId: session._id,
+    },
+  });
 };
 
 
@@ -21,6 +54,10 @@ export const logoutUserController = async (req,res) => {
     await logoutUser(req.cookies.sessionId);
   };
 
+   res.clearCookie('sessionId');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
 };
 
 
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,17 +1,59 @@
-
+import createHttpError from "http-errors";
+import { UserCollections } from "../db/models/user.js";
+import bcrypt from 'bcrypt';
+import { SessionsCollection } from "../db/models/session.js";
+import { FIFTEEN_MINUTES, ONE_DAY } from "../constants/index.js";
+import { randomBytes } from 'crypto';
 
 export const registerUser = async (payload) =>{
+    const user = await UserCollections.findOne({email:payload.email});
 
+    if (user) throw createHttpError("409","Email in use");
+
+    const enxryptedPassord = await bcrypt.hash(payload.password,10);
+
+    return await UserCollections.create({
+        ...payload,
+        password:enxryptedPassord,
+    });
 };
 
 
 export const loginUser = async (payload) => {
+    const user = await UserCollections.findOne({email: payload.email});
 
+     if (!user) {
+    throw createHttpError(401, 'User not Found');
+  }
+
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+   if (!isEqual) {
+    throw createHttpError(401, 'Incorrect password');
+  }
+
+
+
+  await SessionsCollection.deleteOne({ userId: user._id });
+
+   const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+
+  const session = await SessionsCollection.create({
+     userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
+  });
+
+  return session;
 };
 
 
 export const logoutUser = async (sessionId) => {
-
+    await SessionsCollection.deleteOne({ _id: sessionId });
 };
 
 
