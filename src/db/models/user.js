e7e41a4eea2af0dfe3bd91a283561d256bf68022
Mykyta Petrakovskiy <nekitpetr46228@gmail.com>
2025-09-17 10:50:47 +0200

    update userSchema

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -3,9 +3,9 @@ import { model, Schema } from "mongoose";
 
 
 const userSchema = new Schema({
-    name: {type: String,required:true},
-    email: {type:String,required:true,unique:true},
-    password: {type:String,required:true},
+    name: {type: String,required:true,maxlength: [32, 'Name cannot exceed 32 characters'],},
+    email: {type:String,required:true,unique:true,  maxlength: [64, 'Email cannot exceed 64 characters'], match: [/\S+@\S+\.\S+/, 'Email is invalid'],},
+    password: {type:String,required:true, minlength: [8, 'Password must be at least 8 characters'], maxlength: [128, 'Password cannot exceed 128 characters'],},
 },
 {timestamps:true,versionKey:false},
 );
