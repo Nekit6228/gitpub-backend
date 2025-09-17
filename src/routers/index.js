194126abf4e571220ab7f9464d008da92702f6f4
Mykyta Petrakovskiy <nekitpetr46228@gmail.com>
2025-09-17 22:22:35 +0200

    renamed rotes for auth

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import contactsRouter from './contacts.js';
 import authRouter from './auth.js';
 
 
 const router = Router();
 
-router.use('/contacts',contactsRouter);
-router.use('/auth', authRouter);
+
+router.use('api/auth', authRouter);
 
 
 export default router;
