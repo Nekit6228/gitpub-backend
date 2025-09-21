cbff0e498b12ab8d62b1206ad6a5e425b44459a7
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-09-21 21:14:00 +0300

    fix merge conflicts in index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,12 +3,12 @@ import authRouter from './auth.js';
 import diaryRouter from './diary.js';
 import emotionRouter from './emotions.js';
 import usersRouter from './user.js';
-
+import weeksRouter from './weeks.js';
 const router = Router();
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 router.use('/users', usersRouter);
-
+router.use('/weeks', weeksRouter);
 router.use('/diaries', diaryRouter);
 router.use('/emotions', emotionRouter);
 
