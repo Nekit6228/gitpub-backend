8eedf80bea58a97957ba789a8973cae6eed7c9db
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-09-21 20:56:13 +0300

    server conflict

--- src/server.js
+++ src/server.js
@@ -9,10 +9,9 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
-import tasksRouter from './routers/tasks.js';
-
 
 dotenv.config();
+
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const startServer = () => {
@@ -20,21 +19,35 @@ export const startServer = () => {
 
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
+
   app.use(
-    express.json({ type: ['application/json', 'application/vnd.api+json'] }),
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
   );
   app.use(cors());
   app.use(cookieParser());
-  app.use(pino({ transport: { target: 'pino-pretty' } }));
 
-  app.get('/', (_req, res) => res.json({ message: 'Hello World!' }));
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
 
   app.use('/api', router);
-
-  app.use('/api/tasks', tasksRouter);
 
   app.use(notFoundHandler);
   app.use(errorHandler);
 
-  app.listen(PORT, () => console.log(`port ${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`port ${PORT}`);
+  });
 };
